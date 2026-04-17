d82a35b61c636346e17523a8877ae15111f29329
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-04-17 08:20:23 -0600

    update(pipeline): data pipline

--- src/pipeline/run_pipeline.py
+++ src/pipeline/run_pipeline.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
 from src.ingestion.load_data import load_data
 from pathlib import Path
 
@@ -6,6 +8,7 @@ from src.preprocessing.data_cleaning import clean_data
 from src.validation.data_validation import validate_data
 from src.features.select_features import select_features
 from src.features.feature_engineering import transform
+
 from src.utils.logger_config import get_logger
 from src.models.train import train
 from src.evaluation.metrics import evaluate
@@ -28,3 +31,16 @@ if __name__ == "__main__":
     model, X_test, y_test = train(df)
 
     evaluate(model, X_test, y_test)
+
+
+    model, X_test, y_test = train(df)
+
+    # Generated predictions
+    y_pred = model.predict(X_test)
+
+    # Get metrics
+    print(classification_report(y_test, y_pred))
+
+
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    plt.show()
